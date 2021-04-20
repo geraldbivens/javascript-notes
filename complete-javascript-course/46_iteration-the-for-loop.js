c769cbe2ff0df3e3265f2f5d0b5a510b28debad1
Gerald Bivens <geraldbivens@gmail.com>
2021-04-20 13:07:43 -0600

    Add iteration-the-for-loop.

---
+++ complete-javascript-course/46_iteration-the-for-loop.js
@@ -0,0 +1,7 @@
+// The Complete JavaScript Course 2021
+// JavaScript Fundamentals — Part 2
+// 46. Iteration: The for Loop
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Bench press repetition ${rep}`);
+}
