e43c061de41b512d4ca9610ee24896e23efabab7
Gerald Bivens <geraldbivens@gmail.com>
2021-04-20 07:24:06 -0600

    Update dot vs bracket notation.

--- complete-javascript-course/43_dot-vs-bracket-notation.js
+++ complete-javascript-course/43_dot-vs-bracket-notation.js
@@ -14,17 +14,29 @@ const hollis = {
 console.log(hollis.lastName);
 console.log(hollis["lastName"]);
 
-// Bracket notation allows you to use an expression
+// Bracket notation allows you to use an expression (dot notation does not)
 const nameKey = "Name";
 console.log(hollis["first" + nameKey]);
 console.log(hollis["last" + nameKey]);
 
-// Adding to the object using both dot and bracket notation
-hollis.location = "Denver";
+const info = prompt(
+  "What would you like to know about Hollis? Choose between firstName, lastName, age, job, and friends."
+);
+
+if (hollis[info]) {
+  console.log(hollis[info]);
+} else {
+  console.log(
+    `That info isn't available. Please choose between firstName, lastName, age, job, and friends.`
+  );
+}
+
+// Adding new properties to an object using both dot and bracket notation
+hollis.location = "Northglenn";
 hollis["twitter"] = "@hollisarchibold";
 
 // Challenge
-// "Hollis has 3 friends, and her best friend is named Molly."
+// Log the following sentence to the console, dynamically: "Hollis has 3 friends, and her best friend is named Molly."
 console.log(
   `${hollis.firstName} has ${hollis.friends.length} friends, and her best friend is named ${hollis.friends[0]}.`
 );
