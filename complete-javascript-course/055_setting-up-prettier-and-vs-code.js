d711601f13d04cae6b0d396cabd3877e0e76e88d
Gerald Bivens <geraldbivens@gmail.com>
2021-05-19 10:12:55 -0600

    Add setting up prettier and vs code.

---
+++ complete-javascript-course/055_setting-up-prettier-and-vs-code.js
@@ -0,0 +1,12 @@
+// The Complete JavaScript Course 2021
+// Developer Skills & Editor Setup
+// 55. Setting up Prettier and VS Code
+
+// Installing the Prettier extension
+// - Find the extension by searching for it using the search bar
+// - Enable the extension
+// - Go to Preferences/Settings and search for the Default Formatter, then choose Prettier as the default
+// - Search for Format on Save and make sure that it is checked
+
+// Configuring the Prettier extension
+// - Check out the docs on the Prettier website for configuration options: https://prettier.io/docs/en/index.html
