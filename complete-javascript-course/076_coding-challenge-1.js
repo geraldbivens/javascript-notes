27a861adce83305a0b2acac2a0baea123b195e5c
Gerald Bivens <geraldbivens@gmail.com>
2021-05-22 13:35:32 -0600

    Add lesson 76 coding challenge 1.

---
+++ complete-javascript-course/076_coding-challenge-1.js
@@ -0,0 +1,30 @@
+// The Complete JavaScript Course 2021
+// JavaScript in the Browser: DOM and Events Fundamentals
+// 76. Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+playAgainButton.addEventListener("click", function () {
+  secretNumber.textContent = "?";
+  secretNumberValue = Math.trunc(Math.random() * 20) + 1;
+  guessInput.value = "";
+  message.textContent = "Start guessing...";
+  score = 20;
+  currentScore.textContent = score;
+  background.style.backgroundColor = "#222";
+  secretNumber.style.width = "15rem";
+});
+
+// Refactor
+// playAgainButton.addEventListener("click", function () {
+//   window.location.reload();
+// });
