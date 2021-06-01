fa437c28c7b337b64ffe0271c309c5d1a1de152e
Gerald Bivens <geraldbivens@gmail.com>
2021-06-01 08:41:07 -0600

    Add notes and screenshots for complete js course lessons 89 and 96.

---
+++ complete-javascript-course/089_high-level-overview-of-js.js
@@ -0,0 +1,55 @@
+// The Complete JavaScript Course 2021
+// How JavaScript Works Behind the Scenes
+// 89. An High-Level Overview of JavaScript
+
+// JavaScript
+// JavaScriptis a high-level, object-oriented, multi-paradigm programming level.
+
+// JavaScript
+// JavaScript is a high-level, prototype-based object-oriented, multi-paradigm, interpreted or just-in-time compiled, dynamic, single-threaded, garbage-collected programming language with first-class functions and a non-blocking event loop concurrency model.
+
+// Nine topics covered in this lecture
+// - High-level
+// - Garbage-collected
+// - Interpreted or just-in-time compiled
+// - Multi-paradigm
+// - Prototype-based object-oriented
+// - First-class functions
+// - Dynamic
+// - Single-threaded
+// - Non-blocking event loop
+
+// High-level
+// Software programs require hardware resources to run (i.e., memory and the CPU). Whether these resources are allocated manually or automatically is the difference between a programming language being categorized as low-level or high-level. Low-level languages, like C, allow the developer to allocate these resources manually, while high-level languages, like JavaScript, allocate these resources automatically.
+
+// Garbage collection
+// Garbage collection is an algorithm in the JavaScript engine. It handles the allocation of memory by automatically removing old, unused objects. (Is this all that it does?)
+
+// Interpreted or just-in-time compiled
+// Computer processors only understand 0s and 1s, or machine code. Compiling is the process of converting the code written in a specific language to machine code so that it can be understood by the processor. In the case of JavaScript, this process takes place within the JavaScript engine, or behind the scenes. (Compiling is also referred to as 'interpreting.')
+
+// Multi-paradigm
+// Paradigm: An approach and mindset of structuring code, which will direct your coding style and technique.
+
+// Three popular paradigms
+// - Procedural programming
+// This is paradigm that we have been using thus far in the course—that is, we've been organizing the code in a linear way, with some functions thrown inbetween.
+
+// - Object-oriented programming (OOP)
+
+// - Fuctional programming (FP)
+
+// Programming paradigms can be classified as either imperative or declarative.
+
+// Prototype-based object-oriented
+// Nearly everything in JS is an object, with the exception of primitive values such as numbers and strings.
+// (https://developer.mozilla.org/en-US/docs/Glossary/Primitive)
+// What does it mean to be an object in JS? It seems to mean that objects are based on prototypes, which we can also think of as a blueprint or a template (or a class?).
+// Instances of a prototype inherit the methods from the prototype. This is prototypal inheritance.
+
+// First-class functions
+// In a language with first-class functions, functions are simply treated as variables. We can pass them into other functions, and return them from functions.
+
+// Dynamic (Dynamically typed)
+// Data types are not assigned to variables, and the data type can be changed when we reassign a variable.
+// Contrasted with strongly typed languages.
---
+++ complete-javascript-course/096_this-keyword.js
@@ -0,0 +1,5 @@
+// The Complete JavaScript Course 2021
+// How JavaScript Works Behind the Scenes
+// 96. The this keyword
+
+// this keyword/variable: Special variable that is created for every execution context (every function). Takes the value of (points to) the "owner" of the function in which the 'this' keyword is used.
